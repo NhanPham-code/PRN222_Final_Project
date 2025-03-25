df1ff3f534608a88f4dc929c4b68ed84f8de8c2f
Nhi <nhitrancaouyen@gmail.com>
2025-03-25 16:48:54 +0700

    search

--- Razor_UI/wwwroot/js/site.js
+++ Razor_UI/wwwroot/js/site.js
@@ -23,62 +23,62 @@ connection.on("loadProduct", function () {
     location.href = '/Admin/ProductManagement'
 });
 
-    //payment status
-    connection.on("ReceivePaymentUpdate", function (orderId, paymentStatus) {
-        // Find the row with the matching order ID and update its payment status
-        $(`select[data-order-id="${orderId}"]`).val(paymentStatus);
+//payment status
+connection.on("ReceivePaymentUpdate", function (orderId, paymentStatus) {
+    // Find the row with the matching order ID and update its payment status
+    $(`select[data-order-id="${orderId}"]`).val(paymentStatus);
+});
+
+//order status
+connection.on("ReceiveOrderStatusUpdate", function (orderId, orderStatus) {
+    $(`select.order-status[data-order-id="${orderId}"]`).val(orderStatus);
+});
+
+$('.payment-status').change(function () {
+    const orderId = $(this).data('order-id');
+    const paymentStatus = $(this).val();
+
+    $.ajax({
+        url: window.location.pathname,
+        type: 'POST',
+        data: {
+            OrderId: orderId,
+            PaymentStatus: paymentStatus,
+            __RequestVerificationToken: $('input[name="__RequestVerificationToken"]').val()
+        },
+        success: function (result) {
+            console.log("Payment status updated successfully");
+        },
+        error: function (error) {
+            console.error("Error updating payment status:", error);
+        }
     });
+});
 
-    //order status
-    connection.on("ReceiveOrderStatusUpdate", function (orderId, orderStatus) {
-        $(`select.order-status[data-order-id="${orderId}"]`).val(orderStatus);
+$('.order-status').change(function () {
+    const orderId = $(this).data('order-id');
+    const orderStatus = $(this).val();
+
+    $.ajax({
+        url: window.location.pathname,
+        type: 'POST',
+        data: {
+            OrderId: orderId,
+            OrderStatus: orderStatus,
+            __RequestVerificationToken: $('input[name="__RequestVerificationToken"]').val()
+        },
+        success: function (result) {
+            console.log("Order status updated successfully");
+        },
+        error: function (error) {
+            console.error("Error updating order status:", error);
+        }
     });
+});
 
-    $('.payment-status').change(function () {
-        const orderId = $(this).data('order-id');
-        const paymentStatus = $(this).val();
-
-        $.ajax({
-            url: window.location.pathname,
-            type: 'POST',
-            data: {
-                OrderId: orderId,
-                PaymentStatus: paymentStatus,
-                __RequestVerificationToken: $('input[name="__RequestVerificationToken"]').val()
-            },
-            success: function (result) {
-                console.log("Payment status updated successfully");
-            },
-            error: function (error) {
-                console.error("Error updating payment status:", error);
-            }
-        });
-    });
-
-    $('.order-status').change(function () {
-        const orderId = $(this).data('order-id');
-        const orderStatus = $(this).val();
-
-        $.ajax({
-            url: window.location.pathname,
-            type: 'POST',
-            data: {
-                OrderId: orderId,
-                OrderStatus: orderStatus,
-                __RequestVerificationToken: $('input[name="__RequestVerificationToken"]').val()
-            },
-            success: function (result) {
-                console.log("Order status updated successfully");
-            },
-            error: function (error) {
-                console.error("Error updating order status:", error);
-            }
-        });
-    });
-
-    connection.on("DeleteUser", function () {
-        location.href = '/Admin/AccountManagement'
-    });
+connection.on("DeleteUser", function () {
+    location.href = '/Admin/AccountManagement'
+});
 
 
 connection.start().catch(function (err) {
